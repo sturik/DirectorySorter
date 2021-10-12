192fbeaddc6933e76e43cc07f99942f095d7562a
sturik <cam.sture@gmail.com>
2021-10-11 21:28:23 -0700

    renamed files from 'downloads' to 'directory'

---
+++ dir_cleaner_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /Users/sturik/devel/Utilities/downloads_sorter
+. /Users/sturik/.local/share/virtualenvs/downloads_sorter-_0zG4P34/bin/activate
+python downloads_sorter.py
+deactivate
